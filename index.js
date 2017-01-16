38cc672e3ddcb94dd94295552ceadad7b132795a
Ryan Lindskog <rlindskog@Ryans-MacBook-Air.local>
2017-01-16 14:35:49 -0800

    added session support

--- index.js
+++ index.js
@@ -1,73 +1,120 @@
 const modernizr = require('modernizr')
+const bodyParser = require('body-parser')
 
 function modernizrExpress (options={}) {
   return (req, res, next) => {
-    let cookieName = options.cookieName || 'modernizr' // default to 'modernizr'
-
-    if (req.cookies === undefined) {
-      throw (Error(`Cannot read property 'cookieName' of undefined
-
-      Make sure you have cookie-parser installed, and is used before modernizr-server.
-
-      Try:
-
-      npm install cookie-parser
-
-      and
-
-      const cookieParser = require('cookie-parser')
-      const modernizrServer = require('modernizr-server')
-
-      app.use(cookieParser())
-      app.use(modernizrServer())
-
-      ...
-
-      see https://www.github.com/rlindskog/modernizr-server/ for more information
-      `
-    ))
-    }
-    if (req.cookies[cookieName]) {
-      req.cookies[cookieName] = JSON.parse(req.cookies[cookieName])
-      // cookie already exists, continue
-      next()
+    if (options.storageMethod == 'cookie') {
+      handleCookie(req, res, next, options)
+    } else if (options.storageMethod === 'session') {
+      bodyParser.urlencoded({ extended: false })(req, res, handleSession(req, res, next, options))
     } else {
-      let expires = options.expires || 0 // default to expires=Session
+      handleCookie(req, res, next, options) // default
+    }
+  }
+}
+
+function handleCookie(req, res, next, options) {
+  let storageName = options.storageName || 'modernizr' // default to 'modernizr'
+  if (req.cookies === undefined) {
+    throw (Error(noCookieMessage(req.cookies[storageName])))
+  }
+  if (req.cookies[storageName]) {
+    req.cookies[storageName] = JSON.parse(req.cookies[storageName])
+    // cookie already exists, continue
+    next()
+  } else {
+    let expires = options.expires || 0 // default to expires=Session
+    if (options.build) {
+      let build = options.build
+      let scriptToSend = modernizrJsBuild(build, storageName, expires, setModernizrCookieJs)
+      res.send(scriptToSend)
+    } else if (options.config) {
+      // use the config and make a build...
+      modernizr.build(options.config, build => {
+        let scriptToSend = modernizrJsBuild(build, storageName, setModernizrCookieJs, expires)
+        res.send(scriptToSend)
+      })
+    } else {
+      // use a pre-configured CDN
+      let cdn = options.cdn || 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'
+      let scriptToSend = modernizrJsCDN(cdn, storageName, setModernizrCookieJs, expires)
+      res.send(scriptToSend)
+    }
+  }
+}
+
+function handleSession(req, res, next, options) {
+  let storageName = options.storageName || 'modernizr' // default to 'modernizr'
+  req.session.expires = options.expires || req.session.expires
+  return () => {
+    if (req.session === undefined) {
+      throw (Error('Something went wrong'))
+      // throw (Error(noCookieMessage(req.session[storageName])))
+    }
+    if (req.session[storageName]) {
+      next()
+    } else if (req.query[storageName] ) { // the request from the javascript
+      req.session[storageName] = JSON.parse(req.query[storageName])
+      next()
+    } else { // the original request
       if (options.build) {
         let build = options.build
-        let scriptToSend = modernizrJsBuild(build, cookieName, expires)
+        let scriptToSend = modernizrJsBuild(build, storageName, setModernizrSessionJs)
         res.send(scriptToSend)
       } else if (options.config) {
         // use the config and make a build...
         modernizr.build(options.config, build => {
-          let scriptToSend = modernizrJsBuild(build, cookieName, expires)
+          let scriptToSend = modernizrJsBuild(build, storageName, setModernizrSessionJs)
           res.send(scriptToSend)
         })
       } else {
         // use a pre-configured CDN
         let cdn = options.cdn || 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'
-        let scriptToSend = modernizrJsCDN(cdn, cookieName, expires)
+        let scriptToSend = modernizrJsCDN(cdn, storageName, setModernizrSessionJs)
         res.send(scriptToSend)
       }
     }
   }
 }
 
-function modernizrJsBuild (modernizrBuild, cookieName, expires) {
+function noCookieMessage(storageName) {
+  return `Cannot read property '${storageName}' of undefined
+
+  Make sure you have cookie-parser installed, and is used before modernizr-server.
+
+  Try:
+
+  npm install cookie-parser
+
+  and
+
+  const cookieParser = require('cookie-parser')
+  const modernizrServer = require('modernizr-server')
+
+  app.use(cookieParser())
+  app.use(modernizrServer())
+
+  ...
+
+  see https://www.github.com/rlindskog/modernizr-server/ for more information
+  `
+}
+
+function modernizrJsBuild (modernizrBuild, storageName, setModernizr, expires=0) {
   return `
     <script type="text/javascript">${modernizrBuild}</script>
-    <script type="text/javascript">${setModernizrJs(cookieName, expires)}</script>
+    <script type="text/javascript">${setModernizr(storageName, expires)}</script>
   `
 }
 
-function modernizrJsCDN (modernizrCDN, cookieName, expires) {
+function modernizrJsCDN (modernizrCDN, storageName, setModernizr, expires=0) {
   return `
     <script type="text/javascript" src="${modernizrCDN}"></script>
-    <script type="text/javascript">${setModernizrJs(cookieName, expires)}</script>
+    <script type="text/javascript">${setModernizr(storageName, expires)}</script>
   `
 }
 
-function setModernizrJs (cookieName, expires) {
+function setModernizrCookieJs (storageName, expires) {
   let expiresStr = expires === 0 ? `let expiresDate = 0` : `
     let now = new Date()
     now.setTime(now.getTime()+${expires})
@@ -76,9 +123,24 @@ function setModernizrJs (cookieName, expires) {
   return `
     ${expiresStr}
     try {
-      document.cookie = '${cookieName}=' + JSON.stringify(Modernizr) + '; expires=' + expiresDate + ';'
-      document.location.reload()
+      document.cookie = '${storageName}=' + JSON.stringify(Modernizr) + '; expires=' + expiresDate + ';';
+      document.location.reload();
     } catch(e) {}
+  `
+}
+
+function setModernizrSessionJs (storageName, expires) {
+  return `
+    try {
+      function sendModernizr() {
+        let xhttp = new XMLHttpRequest();
+        let payload = JSON.stringify(Modernizr)
+        xhttp.open("GET", document.location.pathname + '?${storageName}=' + payload, true);
+        xhttp.send();
+        document.location.reload();
+      }
+      sendModernizr();
+    } catch(e) {};
   `
 }
 
